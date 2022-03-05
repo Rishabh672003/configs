a5f0aff460ab1ca19c84cb52f6961a19e1ef832e
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2022-03-05 21:32:32 +0530

    Update rate-mirrors-arch.sh

--- rate-mirrors-arch.sh
+++ rate-mirrors-arch.sh
@@ -1,6 +1,8 @@
-export TMPFILE="$(mktemp)"; \
-   rate-mirrors --allow-root --save=$TMPFILE arch --max-delay=21600 \
+ export TMPFILE="$(mktemp)"; \
+         rate-mirrors --allow-root --save=$TMPFILE arch --max-delay=21600 \
       && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
       && sudo mv $TMPFILE /etc/pacman.d/mirrorlist \
-      && sudo chmod a+wr /etc/pacman.d/mirrorlist \
-
+      && sudo paccache -rk3; yay -Sc --aur --noconfirm \
+      && eos-rankmirrors \
+      && sudo chmod a+r /etc/pacman.d/mirrorlist /etc/pacman.d/endeavouros-mirrorlist \
+      && sudo bash /home/rishabh/hosts-maker/hosts-maker.sh

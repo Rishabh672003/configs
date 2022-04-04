7e72924361c966ebbde6d0ca826e757ad60b0d46
Rishabh672003 <jharishabh2k3@gmail.com>
2022-04-04 14:48:33 +0530

    remove brave a Please enter the commit message for your changes. Lines starting

--- Brave-Browser/Default/Extensions/bnchicpgbdgahiecgofdabidjihblaff/1.10.9_0/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-        chrome.runtime.setUninstallURL("https://forms.gle/R62LbY8D7aRDs8PR9");
-    }
-});

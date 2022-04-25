f75bc34e74f05c386358bc9f20c5a96e46827f68
Rishabh <jharishabh2k3@gmail.com>
2022-04-25 22:54:26 +0530

    updated the script

--- mirrors-and-hosts/rate-mirrors-arch.sh
+++ mirrors-and-hosts/rate-mirrors-arch.sh
@@ -3,7 +3,7 @@
 	 rate-mirrors --allow-root --save=$TMPFILE arch --max-delay=21600 \
       && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
       && sudo mv $TMPFILE /etc/pacman.d/mirrorlist \
-      && sudo chmod 777 /etc/pacman.d/mirrorlist 
+      && sudo chmod 777 /etc/pacman.d/mirrorlist  \
       && sudo bash /home/rishabh/projects/my-configs/mirrors-and-hosts/hosts-maker.sh 
 
 

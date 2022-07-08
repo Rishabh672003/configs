400688db30f7a6145cdd0796629bafad87f631eb
Rishabh <jharishabh2k3@gmail.com>
2022-07-08 09:14:48 +0530

    updated the api

--- awesome/user_variables.lua
+++ awesome/user_variables.lua
@@ -2,11 +2,11 @@ return {
 	widget = {
 		weather = {
 			--- API Key
-			key = "fc615b3e335a1ab6b235ba90a195c619",
+			key = "",
 			--- Coordinates
 			coordinates = {
-				"19.4667", --- lat
-				"72.8", --- lon
+				"", --- lat
+				"", --- lon
 			},
 		},
 

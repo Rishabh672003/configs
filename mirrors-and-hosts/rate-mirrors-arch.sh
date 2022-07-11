02d34bf063522f8f5466b63bb406788acc60febb
Rishabh <jharishabh2k3@gmail.com>
2022-07-11 16:08:58 +0530

    updated stuff

--- mirrors-and-hosts/rate-mirrors-arch.sh
+++ mirrors-and-hosts/rate-mirrors-arch.sh
@@ -2,9 +2,9 @@
 
 TMPFILE="$(mktemp)"
 export TMPFILE
-sudo true
-rate-mirrors --disable-comments --allow-root --save="$TMPFILE" arch --max-delay=21600 &&
-sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup &&
-sudo mv "$TMPFILE" /etc/pacman.d/mirrorlist &&
-sudo chmod 777 /etc/pacman.d/mirrorlist &&
-sudo bash /home/rishabh/projects/hosts/hosts-maker.sh
+    sudo true; \
+    rate-mirrors --disable-comments --allow-root --save="$TMPFILE" arch --max-delay=21600 \
+    && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
+    && sudo mv "$TMPFILE" /etc/pacman.d/mirrorlist \
+    && sudo chmod 777 /etc/pacman.d/mirrorlist  \
+    && sudo bash /home/rishabh/projects/hosts/hosts-maker.sh
